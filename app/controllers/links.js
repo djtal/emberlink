26fa5c8a0f1c0d0e525b96ba3978273e342a1e13
Guillaume Garcera <guillaume.garcera@gmail.com>
2014-08-13 21:57:50 +0200

    Sort ink by revrese creation date

--- app/controllers/links.js
+++ app/controllers/links.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   itemController: 'link',
   needs: ['application'],
+  sortProperties: ['created_at'],
+  sortAscending: false,
 
   isAuthenticated: Ember.computed.alias('controllers.application.isAuthenticated'),
 });
